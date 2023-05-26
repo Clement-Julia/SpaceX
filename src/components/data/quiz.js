f7c6ba5e85cd1913bf7831b55ae291129c6feb45
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-05-26 21:10:40 +0200

    fix: quizz

--- src/components/data/quiz.js
+++ src/components/data/quiz.js
@@ -1,135 +1,165 @@
 const quizz = [
-    {
+	{
 		type: 'True or False',
 		quizz: [
 			{
 				title: 'Quiz NASA',
 				questions: [
 					{
+						id: 1,
 						question: 'La NASA a été fondée en 1958.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 2,
 						question: 'Le premier homme à marcher sur la Lune était Neil Armstrong.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 3,
 						question: "La NASA est l'agence spatiale américaine.",
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 4,
 						question: 'Le télescope spatial Hubble a été lancé par la NASA.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 5,
 						question: 'La NASA a réalisé la mission Apollo 13 qui a été un échec.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 6,
 						question: 'La NASA est responsable du programme de la Station spatiale internationale (ISS).',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 7,
 						question: 'La NASA a envoyé des rovers sur Mars pour étudier la planète.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 8,
 						question: 'La NASA a envoyé des humains sur Mars.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 9,
 						question: 'La NASA a envoyé des sondes Voyager pour explorer les confins du système solaire.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 10,
 						question: 'La NASA est responsable du programme Artemis qui vise à renvoyer des astronautes sur la Lune.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
@@ -141,130 +171,160 @@ const quizz = [
 				title: 'Quiz Exploration spatiale',
 				questions: [
 					{
+						id: 1,
 						question: "L'être humain a déjà marché sur Mars.",
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 2,
 						question: 'La sonde Voyager 1 a quitté le système solaire.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 3,
 						question: 'La mission Apollo 11 a été la première à envoyer des humains sur la Lune.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 4,
 						question: 'Le rover Curiosity a été envoyé sur Mars par la NASA.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 5,
 						question: 'Le télescope spatial James Webb sera le successeur du télescope Hubble.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 6,
 						question: 'La mission Rosetta a permis de poser un atterrisseur sur la comète Tchourioumov-Guérassimenko.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 7,
 						question: 'Le vaisseau spatial New Horizons a survolé la planète Pluton en 2015.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 8,
 						question: "La mission Mars Science Laboratory a été lancée par l'ESA (Agence spatiale européenne).",
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 9,
 						question: "Le télescope spatial Chandra observe l'univers dans le domaine des rayons X.",
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 10,
 						question: 'Le satellite Kepler a été lancé pour rechercher des exoplanètes.',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vrai',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Faux',
 								correct: false,
 							},
@@ -281,210 +341,260 @@ const quizz = [
 				title: 'Quiz SpaceX',
 				questions: [
 					{
+						id: 1,
 						question: 'Quel entrepreneur a fondé SpaceX ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Elon Musk',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Jeff Bezos',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Richard Branson',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Tim Cook',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 2,
 						question: 'Quelle est la première fusée réutilisable développée par SpaceX ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Falcon 1',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Falcon 9',
 								correct: true,
 							},
 							{
+								id: 3,
 								answer: 'Falcon Heavy',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Starship',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 3,
 						question: 'Quel est le nom du vaisseau spatial développé par SpaceX pour transporter des astronautes vers la Station spatiale internationale (ISS) ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Crew Dragon',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Starliner',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Dream Chaser',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Orion',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 4,
 						question: 'Quel est le nom du projet de SpaceX visant à établir une colonie humaine sur Mars ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Red Planet',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Mars One',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Mars Base Alpha',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Starship/Super Heavy',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 5,
 						question: 'Quel est le nom du premier touriste spatial qui a réservé un vol autour de la Lune avec SpaceX ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Elon Musk',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Jeff Bezos',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Yusaku Maezawa',
 								correct: true,
 							},
 							{
+								id: 4,
 								answer: 'Richard Branson',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 6,
 						question: "Quel est le nom du propulseur réutilisable de SpaceX qui est utilisé pour le retour et l'atterrissage des étages de fusée ?",
 						answers: [
 							{
+								id: 1,
 								answer: 'Dragon',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Merlin',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Starhopper',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Falcon 9',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 7,
 						question: "Quelle est la première mission spatiale habitée de SpaceX à avoir transporté des astronautes vers l'ISS ?",
 						answers: [
 							{
+								id: 1,
 								answer: 'Crew-1',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Demo-2',
 								correct: true,
 							},
 							{
+								id: 3,
 								answer: 'Inspiration4',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Crew-2',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 8,
 						question: "Quel est le nom du projet de SpaceX visant à fournir un accès Internet haut débit depuis l'espace ?",
 						answers: [
 							{
+								id: 1,
 								answer: 'Starlink',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'GalaxyLink',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'SatelliteNet',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'SpaceNet',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 9,
 						question: 'Quel est le nom de la première mission de SpaceX à envoyer des civils en orbite sans astronautes professionnels à bord ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'Crew-1',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Demo-2',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Inspiration4',
 								correct: true,
 							},
 							{
+								id: 4,
 								answer: 'Crew-2',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 10,
 						question: 'Quelle entreprise a été la première à envoyer une fusée réutilisable en orbite terrestre ?',
 						answers: [
 							{
+								id: 1,
 								answer: 'SpaceX',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Blue Origin',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Virgin Galactic',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Roscosmos',
 								correct: false,
 							},
@@ -496,210 +606,260 @@ const quizz = [
 				title: 'Quiz Astronomie',
 				questions: [
 					{
+						id: 1,
 						question: 'La Voie lactée est une :',
 						answers: [
 							{
+								id: 1,
 								answer: 'Galaxie spirale',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Galaxie elliptique',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Galaxie irrégulière',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Galaxie naine',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 2,
 						question: 'La planète la plus proche du Soleil est :',
 						answers: [
 							{
+								id: 1,
 								answer: 'Vénus',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Mars',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Mercure',
 								correct: true,
 							},
 							{
+								id: 4,
 								answer: 'Jupiter',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 3,
 						question: 'Le soleil est une étoile de type :',
 						answers: [
 							{
+								id: 1,
 								answer: 'Naine blanche',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Géante rouge',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Naine rouge',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Séquence principale',
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 4,
 						question: "La constellation du Grand Chien contient l'étoile la plus brillante du ciel, nommée :",
 						answers: [
 							{
+								id: 1,
 								answer: 'Sirius',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Véga',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Polaris',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Bételgeuse',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 5,
 						question: "Un astéroïde est un petit corps rocheux qui orbite autour du Soleil. Parmi les propositions suivantes, lequel n'est PAS un astéroïde ?",
 						answers: [
 							{
+								id: 1,
 								answer: 'Bennu',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Cérès',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Io',
 								correct: true,
 							},
 							{
+								id: 4,
 								answer: 'Vesta',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 6,
 						question: "La nébuleuse d'Orion est une :",
 						answers: [
 							{
+								id: 1,
 								answer: 'Nébuleuse planétaire',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Nébuleuse obscure',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Nébuleuse rémanente de supernova',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: "Région de formation d'étoiles",
 								correct: true,
 							},
 						],
 					},
 					{
+						id: 7,
 						question: 'Le nombre de planètes dans notre système solaire est :',
 						answers: [
 							{
+								id: 1,
 								answer: 'Huit',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Neuf',
 								correct: true,
 							},
 							{
+								id: 3,
 								answer: 'Dix',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Onze',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 8,
 						question: 'Le télescope spatial Kepler a été utilisé pour rechercher :',
 						answers: [
 							{
+								id: 1,
 								answer: 'Des comètes',
 								correct: false,
 							},
 							{
+								id: 2,
 								answer: 'Des supernovas',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Des exoplanètes',
 								correct: true,
 							},
 							{
+								id: 4,
 								answer: 'Des astéroïdes',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 9,
 						question: "La ceinture d'astéroïdes est située entre les planètes :",
 						answers: [
 							{
+								id: 1,
 								answer: 'Terre et Mars',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: 'Mars et Jupiter',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: 'Jupiter et Saturne',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: 'Saturne et Uranus',
 								correct: false,
 							},
 						],
 					},
 					{
+						id: 10,
 						question: 'La comète de Halley revient près de la Terre tous les :',
 						answers: [
 							{
+								id: 1,
 								answer: '76 ans',
 								correct: true,
 							},
 							{
+								id: 2,
 								answer: '88 ans',
 								correct: false,
 							},
 							{
+								id: 3,
 								answer: '100 ans',
 								correct: false,
 							},
 							{
+								id: 4,
 								answer: '120 ans',
 								correct: false,
 							},
@@ -708,7 +868,7 @@ const quizz = [
 				],
 			},
 		],
-	}
+	},
 ]
 
 export default quizz
--- src/components/pages/QuizTest.js
+++ src/components/pages/QuizTest.js
@@ -1,34 +1,145 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import '../../assets/style/components/History.css'
 import Quizz from '../data/quiz'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+	return array
+}
 
 const QuizTest = () => {
 	const [data, setData] = useState([])
+	const [isPlaying, setIsPlaying] = useState(true)
 	const { id, type } = useParams()
 	const navigate = useNavigate()
 
 	let quizz = Quizz[type]['quizz'][id]
+	quizz.questions = shuffleArray(quizz.questions);
+	let mark = 0
+	let completed = false
 
 	if (!quizz) {
 		navigate('/quiz/')
 		toast.error(`Aucun quiz trouvé`)
 	}
 
+	/**
+	 * The function saves answers to a dataset based on the target's parent and answer data.
+	 * @param e - The parameter "e" is an event object that is passed to the function when it is called.
+	 * It contains information about the event that triggered the function, such as the target element and
+	 * any data attributes associated with it. In this case, the function is likely being used as an event
+	 * handler for a
+	 */
+	const saveAnswers = (e) => {
+		data[e.target.dataset.parent] = e.target.dataset.answer
+		setData(data)
+	}
+
+	/**
+	 * The function shows the result of a quiz and displays a toast message based on the user's score.
+	 */
+	const showResult = () => {
+		completed = true
+		setIsPlaying(false)
+
+		quizz.questions.map((quiz, i) => {
+			quiz.answers.map((answer, a) => {
+				let element = document.querySelector(`[data-parent='${quiz.id}'][data-answer='${answer.id}'][data-label='1']`)
+				if (answer.correct == true) {
+					console.log(answer.answer);
+					console.log(element);
+					console.log("")
+					element.classList.add('text-green')
+					if (answer.id == data[quiz.id]) {
+						mark++
+					}
+				} else {
+					element.classList.add('text-red')
+				}
+			})
+		})
+
+		if (mark >= 0 && mark <= 4) {
+			toast.error(`Vous avez eu ${mark}/10`)
+		} else if (mark > 4 && mark < 8) {
+			toast.warning(`Vous avez eu ${mark}/10`)
+		} else {
+			toast.success(`Vous avez eu ${mark}/10`)
+		}
+	}
+
+	/**
+	 * The function renders a countdown timer with animation effects.
+	 * @returns The function `renderTime` returns a JSX element that displays the remaining time. It also
+	 * uses `useRef` and `useState` hooks to keep track of the current and previous time values, and to
+	 * trigger a re-render when the time is up.
+	 */
+	const renderTime = ({ remainingTime }) => {
+		if (completed) {
+			remainingTime = 0
+		}
+		const currentTime = useRef(remainingTime)
+		const prevTime = useRef(null)
+		const isNewTimeFirstTick = useRef(false)
+		const [, setOneLastRerender] = useState(0)
+
+		if (currentTime.current !== remainingTime) {
+			isNewTimeFirstTick.current = true
+			prevTime.current = currentTime.current
+			currentTime.current = remainingTime
+		} else {
+			isNewTimeFirstTick.current = false
+		}
+
+		if (remainingTime === 0) {
+			setTimeout(() => {
+				setOneLastRerender((val) => val + 1)
+			}, 20)
+		}
+
+		const isTimeUp = isNewTimeFirstTick.current
+
+		return (
+			<div className='time-wrapper'>
+				<div key={remainingTime} className={`time ${isTimeUp ? 'up' : ''}`}>
+					{remainingTime && isPlaying ? remainingTime : `${mark}/10`}
+				</div>
+				{prevTime.current !== null && (
+					<div key={prevTime.current} className={`time ${!isTimeUp ? 'down' : ''}`}>
+						{prevTime.current}
+					</div>
+				)}
+			</div>
+		)
+	}
+
 	return (
-		<div className='container mt-10'>
-			<div>
-				<h1 className='mb-2'>{quizz.title}</h1>
+		<div className='container pt-10'>
+			<div className='pt-5'>
+				<h1 className='mb-4'>{quizz.title}</h1>
+				<div className='timer'>
+					<div id='timer-element' className='timer-wrapper'>
+						<CountdownCircleTimer isPlaying={isPlaying} duration={60} colors={['#69e279', '#f6ad06', '#901403']} colorsTime={[60, 30, 0]} onComplete={showResult}>
+							{renderTime}
+						</CountdownCircleTimer>
+					</div>
+				</div>
 				{quizz.questions.map((quiz, i) => {
+					quiz.answers = shuffleArray(quiz.answers);
 					return (
 						<div className='container pb-3' key={i}>
 							<h4>{quiz.question}</h4>
 							{quiz.answers.map((answer, a) => {
 								return (
 									<div key={a} className='form-check'>
-										<input className="form-check-input" id={`answer-${i}`} name={`answer-${i}`} type='radio' />
-										<label className='form-check-label' htmlFor='flexRadioDefault1'>
+										<input className='form-check-input uneditable-input' id={`answer-${i}`} name={`answer-${i}`} type='radio' onChange={(e) => saveAnswers(e)} data-parent={quiz.id} data-answer={answer.id} />
+										<label className='form-check-label' htmlFor='flexRadioDefault1' data-label='1' data-parent={quiz.id} data-answer={answer.id}>
 											{answer.answer}
 										</label>
 									</div>
@@ -37,6 +148,11 @@ const QuizTest = () => {
 						</div>
 					)
 				})}
+				<div className='d-flex justify-content-center'>
+					<button className='btn btn-primary mb-2' onClick={showResult}>
+						Valider
+					</button>
+				</div>
 			</div>
 		</div>
 	)
